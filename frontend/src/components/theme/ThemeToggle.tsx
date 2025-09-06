9678ea19df369776838a3ca517149e459a1ffb85
tonyedgal <tonyedgal@gmail.com>
2025-09-06 22:51:50 +0100

    feat: animate theme transition

--- frontend/src/components/theme/ThemeToggle.tsx
+++ frontend/src/components/theme/ThemeToggle.tsx
@@ -3,16 +3,19 @@
 import { useTheme } from 'next-themes';
 import { Button } from '@/components/ui/button';
 import { Moon, Sun } from 'lucide-react';
+import {
+  useThemeAnimation,
+  ThemeAnimationType,
+} from '@space-man/react-theme-animation';
 
 export function ThemeToggle() {
-  const { setTheme, theme } = useTheme();
+  const { theme, ref, toggleTheme } = useThemeAnimation({
+    duration: 1000,
+    animationType: ThemeAnimationType.CIRCLE,
+  });
 
   return (
-    <Button
-      variant='outline'
-      size='icon'
-      onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
-    >
+    <Button variant='outline' size='icon' onClick={toggleTheme} ref={ref}>
       {theme === 'light' ? (
         <Sun className='text-foreground size-[1.2rem] transition-all' />
       ) : (
