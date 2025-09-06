b515a795fec7ce83d9e9c1406f970f887970f494
tonyedgal <tonyedgal@gmail.com>
2025-09-06 23:03:24 +0100

    fix: fix the border radius symmetry on image and div

--- frontend/src/app/components/Hero.tsx
+++ frontend/src/app/components/Hero.tsx
@@ -82,14 +82,14 @@ export default function Hero() {
           />
           <div className='inset-shadow-2xs ring-background dark:inset-shadow-white/20 bg-background relative mx-auto max-w-6xl overflow-hidden rounded-2xl border p-1 shadow-lg shadow-zinc-950/15 ring-1'>
             <img
-              className='bg-background relative hidden rounded-2xl dark:block'
+              className='bg-background relative hidden rounded-[12px] dark:block'
               src='/RepoLensDark.webp'
               alt='app screen'
               width='2700'
               height='1440'
             />
             <img
-              className='z-2 border-border/25 relative rounded-2xl border dark:hidden'
+              className='z-2 border-border/25 relative rounded-[12px] border dark:hidden'
               src='/RepoLensLight.webp'
               alt='app screen'
               width='2700'
