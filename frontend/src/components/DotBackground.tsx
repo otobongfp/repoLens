21c77e8c74ac804bb31545dddcee4671ffa42915
tonyedgal <tonyedgal@gmail.com>
2025-09-11 22:46:01 +0100

    refactor: extend component to accept classNames as props

--- frontend/src/components/DotBackground.tsx
+++ frontend/src/components/DotBackground.tsx
@@ -1,6 +1,8 @@
-const DotBackground = () => {
+import { cn } from '@/lib/utils';
+
+const DotBackground = ({ className }: React.ComponentProps<'div'>) => {
   return (
-    <div className='absolute inset-0'>
+    <div className={cn('absolute inset-0', className)}>
       <div className='relative h-full w-full [&>div]:absolute [&>div]:h-full [&>div]:w-full [&>div]:bg-[radial-gradient(var(--border)_1px,transparent_1px)] [&>div]:[background-size:16px_16px]'>
         <div></div>
       </div>
