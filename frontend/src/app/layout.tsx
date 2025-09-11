892c48e70ac5851c94c4eb5592087cc01c600d9e
tonyedgal <tonyedgal@gmail.com>
2025-09-11 22:42:42 +0100

    refactor: remove highlights

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -53,14 +53,6 @@ export default function RootLayout({
 
             <div className='relative grid h-full grid-cols-[2.5rem_auto_2.5rem] xl:grid-cols-[auto_2rem_1200px_2rem_auto]'>
               <div className='relative col-start-2 h-full w-full overflow-y-auto xl:col-start-3'>
-                <div
-                  aria-hidden='true'
-                  className='absolute inset-0 isolate hidden opacity-65 contain-strict lg:block'
-                >
-                  <div className='w-140 h-320 -translate-y-87.5 absolute left-0 top-0 -rotate-45 rounded-full bg-[radial-gradient(68.54%_68.72%_at_55.02%_31.46%,hsla(0,0%,85%,.08)_0,hsla(0,0%,55%,.02)_50%,hsla(0,0%,45%,0)_80%)]'></div>
-                  <div className='h-320 absolute left-0 top-0 w-60 -rotate-45 rounded-full bg-[radial-gradient(50%_50%_at_50%_50%,hsla(0,0%,85%,.06)_0,hsla(0,0%,45%,.02)_80%,transparent_100%)] [translate:5%_-50%]'></div>
-                  <div className='h-320 -translate-y-87.5 absolute left-0 top-0 w-60 -rotate-45 bg-[radial-gradient(50%_50%_at_50%_50%,hsla(0,0%,85%,.04)_0,hsla(0,0%,45%,.02)_80%,transparent_100%)]'></div>
-                </div>
                 {children}
               </div>
 
